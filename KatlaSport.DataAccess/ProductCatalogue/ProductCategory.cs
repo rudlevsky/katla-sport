8c1fa9df85363a065df47a0b5518f11bf21d46cf
rudlevsky <rudlevsky@gmail.com>
2019-01-31 10:02:05 +0300

    Revert "Add Description property to ProductCategory model. Create a new migration AddCategoryDescription."
    This reverts commit 9455305d02d0d1dec0c5552c91324618afd634c1.

--- KatlaSport.DataAccess/ProductCatalogue/ProductCategory.cs
+++ KatlaSport.DataAccess/ProductCatalogue/ProductCategory.cs
@@ -24,10 +24,7 @@ namespace KatlaSport.DataAccess.ProductCatalogue
         /// </summary>
         public string Code { get; set; }
 
-        /// <summary>
-        /// Gets or sets a product category description.
-        /// </summary>
-        public string Description { get; set; }
+        // TODO STEP 1 - Add "Description" property here.
 
         /// <summary>
         /// Gets or sets a value indicating whether a product category is deleted.
